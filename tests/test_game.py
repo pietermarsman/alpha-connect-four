7df073c54b8cf6451a414ad147e358d5bc2aabce
Pieter Marsman <pietermarsman@gmail.com>
2019-08-13 09:10:16 +0200

    Add test for minimax vs greedy

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,5 +1,5 @@
 from game import TwoPlayerGame
-from player import RandomPlayer, GreedyPlayer
+from player import RandomPlayer, GreedyPlayer, MiniMaxPlayer
 from state import State, Color
 
 
@@ -21,6 +21,17 @@ def test_greedy_is_better_than_random():
     state = State.empty()
     player1 = RandomPlayer('player 1')
     player2 = GreedyPlayer('player 2')
+
+    game = TwoPlayerGame(state, player1, player2)
+    game.play()
+
+    assert player2.color == game.current_state.winner
+
+
+def test_minimax_is_better_than_greedy():
+    state = State.empty()
+    player1 = GreedyPlayer('player 1')
+    player2 = MiniMaxPlayer('player 2')
 
     game = TwoPlayerGame(state, player1, player2)
     game.play()

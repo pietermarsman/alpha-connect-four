507c535d93844f1177cd6c1af7ae83843b4070af
Pieter Marsman <pietermarsman@gmail.com>
2019-08-23 13:55:19 +0200

    Improve classifier by sharing weights if the convolution should be rotation-agnostic

--- connect-four/classifier.py
+++ connect-four/classifier.py
@@ -104,58 +104,78 @@ def create_model(input_size, filters, c=10 ** -4, initialization='he_normal'):
 
 def line_convolution(input, filters, l2):
     conv = Conv3D(filters, 1, kernel_regularizer=l2)(input)
-    # todo use same layer for x and y direction
-    permute_x1 = axis_convolution(conv, filters // 3, 0, l2)
-    permute_y1 = axis_convolution(conv, filters // 3, 1, l2)
-    permute_z1 = axis_convolution(conv, filters // 3, 2, l2)
-    permute_xy = plane_convolution(conv, filters // 4, 0, 1, l2)
-    permute_xz = plane_convolution(conv, filters // 4, 0, 2, l2)
-    permute_yz = plane_convolution(conv, filters // 4, 1, 2, l2)
+    permute_x1, permute_y1 = horizontal_axis_convolution(conv, filters // 3, l2)
+    permute_z1 = vertical_axix_convolution(conv, filters // 3, l2)
+    permute_xy = horizontal_plane_convolution(conv, filters // 4, l2)
+    permute_xz, permute_yz = vertical_plane_convolution(conv, filters // 4, l2)
     permute_xyz = box_convolution(conv, filters // 6, l2)
-    concatenate = Concatenate()(
-        [conv, permute_x1, permute_y1, permute_z1, permute_xy, permute_xz, permute_yz, permute_xyz])
+    concatenate = Concatenate()([conv, permute_x1, permute_y1, permute_z1, permute_xy, permute_xz, permute_yz,
+                                 permute_xyz])
     return concatenate
 
 
-def axis_convolution(input, filters, direction, l2):
-    kernel_size = [1, 1, 1]
-    kernel_size[direction] = FOUR
+def horizontal_axis_convolution(input, filters, l2):
+    shared_squash = Conv3D(filters, [4, 1, 1], kernel_regularizer=l2)
 
-    permute_dims = [1, 2, 3, 4]
-    permute_dims.insert(0, permute_dims.pop(direction))
+    squash_x = shared_squash(input)
+    box_x = spread_plane(squash_x, filters, [1, 2, 3, 4])  # x, y, z, f -> x, y, z, f
 
-    reduce = Conv3D(filters, kernel_size, kernel_regularizer=l2)(input)
-    gather = Reshape((FOUR * FOUR * filters,))(reduce)
+    rotate = Permute([2, 1, 3, 4])(input)
+    squash_y = shared_squash(rotate)
+    box_y = spread_plane(squash_y, filters, [2, 1, 3, 4])  # y, x, z, f -> x, y, z, f
+
+    return box_x, box_y
+
+
+def vertical_axix_convolution(input, filters, l2):
+    squash_z = Conv3D(filters, [1, 1, 4], kernel_regularizer=l2)(input)
+    box_z = spread_plane(squash_z, filters, [2, 3, 1, 4])  # z, x, y, f -> x, y, z, f
+    return box_z
+
+
+def spread_plane(input, filters, permute_dims):
+    gather = Reshape((FOUR * FOUR * filters,))(input)
     repeat = RepeatVector(FOUR)(gather)
     spread = Reshape((FOUR, FOUR, FOUR, filters))(repeat)
     permute = Permute(permute_dims)(spread)
     return permute
 
 
-def plane_convolution(input, filters, direction1, direction2, l2):
-    kernel_size = [1, 1, 1]
-    kernel_size[direction1] = FOUR
-    kernel_size[direction2] = FOUR
+def vertical_plane_convolution(input, plane_filters, l2):
+    reduce = Conv3D(plane_filters, 1, kernel_regularizer=l2)(input)
+    shared_squash = Conv3D(plane_filters, [4, 1, 4], kernel_regularizer=l2)
 
-    permute_dims = [1, 2, 3, 4]
-    permute_dims.insert(0, permute_dims.pop(direction2))
-    permute_dims.insert(0, permute_dims.pop(direction1))
+    squash_xz = shared_squash(reduce)
+    box_xz = spread_axis(squash_xz, plane_filters, [1, 3, 2, 4])  # x, z, y, f -> x, y, z, f
 
-    reduce = Conv3D(filters, 1, kernel_regularizer=l2)(input)
-    squash = Conv3D(filters, kernel_size, kernel_regularizer=l2)(reduce)
-    gather = Reshape((FOUR * filters,))(squash)
+    rotate = Permute([2, 1, 3, 4])(reduce)
+    squash_yz = shared_squash(rotate)
+    box_yz = spread_axis(squash_yz, plane_filters, [3, 1, 2, 4])  # y, z, x, f -> x, y, z, f
+
+    return box_xz, box_yz
+
+
+def horizontal_plane_convolution(input, plane_filters, l2):
+    reduce = Conv3D(plane_filters, 1, kernel_regularizer=l2)(input)
+    squash = Conv3D(plane_filters, [4, 4, 1], kernel_regularizer=l2)(reduce)
+    permute = spread_axis(squash, plane_filters, [1, 2, 3, 4])  # x, y, z, f -> x, y, z, f
+    return permute
+
+
+def spread_axis(input, filters, permute_dims):
+    gather = Reshape((FOUR * filters,))(input)
     repeat = RepeatVector(FOUR * FOUR)(gather)
     spread = Reshape((FOUR, FOUR, FOUR, filters))(repeat)
     permute = Permute(permute_dims)(spread)
     return permute
 
 
-def box_convolution(input, filters, l2):
-    reduce = Conv3D(filters, 1, kernel_regularizer=l2)(input)
-    squash = Conv3D(filters, (FOUR, FOUR, FOUR), kernel_regularizer=l2)(reduce)
-    gather = Reshape((filters,))(squash)
+def box_convolution(input, box_filters, l2):
+    reduce = Conv3D(box_filters, 1, kernel_regularizer=l2)(input)
+    squash = Conv3D(box_filters, (FOUR, FOUR, FOUR), kernel_regularizer=l2)(reduce)
+    gather = Reshape((box_filters,))(squash)
     repeat = RepeatVector(FOUR * FOUR * FOUR)(gather)
-    spread = Reshape((FOUR, FOUR, FOUR, filters))(repeat)
+    spread = Reshape((FOUR, FOUR, FOUR, box_filters))(repeat)
     return spread
 
 
